86a2f38c5574c7cd8cb6570b67576d1ac9dca538
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-09-15 16:04:59 +0200

    added basic calendar

---
+++ src/components/BasicCalendar.tsx
@@ -0,0 +1,23 @@
+import { Calendar, toDateId } from "@marceloterreiro/flash-calendar";
+import { useState } from "react";
+import { Text, View } from "react-native";
+
+const today = toDateId(new Date());
+export function BasicCalendar() {
+  const [selectedDate, setSelectedDate] = useState(today);
+  return (
+    <View>
+      <Text>Selected date: {selectedDate}</Text>
+      <Calendar
+        calendarActiveDateRanges={[
+          {
+            startId: selectedDate,
+            endId: selectedDate,
+          },
+        ]}
+        calendarMonthId={today}
+        onCalendarDayPress={setSelectedDate}
+      />
+    </View>
+  );
+}
